7149069e57cc48245e6ec5979bec884a98e8b0a1
Mark Enfermo <markanthony.enfermo@gmail.com>
2023-12-06 21:19:20 +0800

    initialized

---
+++ nvim/lua/options.lua
@@ -0,0 +1,12 @@
+-- options.lua
+-- This file contains the general options for neovim
+
+vim.opt.number = true
+
+vim.opt.mouse = 'a'
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.encoding = 'utf-8'
